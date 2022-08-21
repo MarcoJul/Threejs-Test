4c14a834ba9d16a1c159894368371314ae154082
marco <marco.giuliotti@gmail.com>
2022-08-21 18:36:36 +0200

    set resizing and camera position

--- src/script.js
+++ src/script.js
@@ -17,14 +17,28 @@ scene.add(mesh);
 
 const sizes = {
     width: window.innerWidth,
-    heigh: window.innerHeight,
+    height: window.innerHeight,
 };
+
+window.addEventListener("resize", () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width / sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 // CAMERA
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.heigh, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
+camera.position.y = 2;
+camera.position.x = 2;
 scene.add(camera);
+camera.lookAt(mesh.position);
 
 // RENDERER
 
@@ -32,6 +46,6 @@ const renderer = new THREE.WebGLRenderer({
     canvas,
 });
 
-renderer.setSize(sizes.width, sizes.heigh);
-
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.render(scene, camera);
